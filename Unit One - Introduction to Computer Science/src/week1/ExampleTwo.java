12d4b8779d95f846358dc4698ba6eae44ccf175a
Kaiyan Chu <kchu@bayviewglen.ca>
2021-09-09 14:49:42 -0400

    commit

---
+++ Unit One - Introduction to Computer Science/src/week1/ExampleTwo.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("A"); //A newline
+        System.out.print("B"); //B
+        System.out.print("C");//C
+        System.out.println("D");// D newline
+        System.out.println("E");// E newline
+        
+
+    }
+}
+
+/*
+A
+BCD
+E
+*/
